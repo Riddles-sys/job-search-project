ded6d375c8e63b50904601f79cf651d4b0bd6b60
Riddles <ridwan.arshad@hotmail.com>
2022-11-25 00:52:36 +0000

    added colours to sign in buttons

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d2bad34adeb5c939"; }
+/******/ 		__webpack_require__.h = function() { return "88eaeeddd3f42eca"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
