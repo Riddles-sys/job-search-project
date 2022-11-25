4509000a665d66c36dc89c922efd59d24f444d03
Riddles <ridwan.arshad@hotmail.com>
2022-11-25 00:20:18 +0000

    fixed styling for mobile

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "57edff77ddf882be"; }
+/******/ 		__webpack_require__.h = function() { return "574e5262570565ec"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
